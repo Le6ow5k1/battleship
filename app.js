85bbdefa5cf80f3da344733a0962dad92d33c10e
Le6ow5k1 <bebot@mail.ru>
2013-12-03 01:01:26 +0600

    add more logic

--- app.js
+++ app.js
@@ -1,36 +1,66 @@
 var _ = require('lodash');
 
 
-var userField = document.getElementById('usr');
-var compField = document.getElementById('comp');
+var userField = document.getElementsById('usr');
+var compField = document.getElementsById('comp');
 
 //
-function Game(argument) {
-    this.userGrid = createGrid(userField);
-    this.compGrid = createGrid(compField);
-    this.isOver = false;
-
+function Game(numRows, numCols) {
+    var grid = new Grid(numRows, numCols);
+    this.userField = new Field(userField).draw(grid);
+    this.compField = new Field(compField).draw(grid);
+    // this.isOver = false;
 }
 
-var game = new Game();
+var game = new Game(10, 10);
 
-Game.prototype.getUserGrid = function() {
-    return userGrid;
+Game.prototype.getUserField = function() {
+    return userField;
 };
 
-// Создает сетку из клеток, клетка представляет собой div элемент.
-// Возвращяет двумерный массив, в котором каждый элемент это клетка.
-function Grid(field, numRows, numCols) {
+Game.prototype.getCompField = function() {
+    return compField;
+};
+
+Game.prototype.isOver = function() {
+    
+};
+
+function Field(field) {
+    // Создает игровое поле, которое представляет собой сетку из клеток
+    // клетка-это div элемент, с соответствующим именем класса вида <y_x i>
+    // где y - у-координата клетки, х - х-координата, а i - тип клетки:\
+    //   'f' - неповрежденная часть корабля
+    //   'x' - поврежденная часть корабля
+    //   'o' - свободная не простреленная клетка
+    //   'p' - свободная простреленная клетка
+    this.draw = function(grid) {
+        grid.map(function(row) {
+            row.map(function(el) {
+                var div = document.createElement('div');
+                div.className = y.toString()+'_'+x.toString()+' '+el;
+                field.appendChild(div);
+                return div;
+            });
+        });
+    };
+
+    // Проверяет остались ли еще корабли
+    this.check = function() {
+        field.some(function(row) {
+            return row.some(function(el) {
+                return (el.className.indexOf(' f') !== -1);
+            });
+        });
+    };
+}
+
+function Grid(numRows, numCols) {
     var grid = new Array(numRows);
     for (var y=0; y < numRows; y++) {
         var row = [];
-        var cell = null;
         for (var x=0; x < numCols; x++) {
-            cell = document.createElement('div');
-            //
-            cell.className = y.toString()+'_'+x.toString()+' o';
-            row[x] = cell;
-            field.appendChild(cell);
+            row[x] = 'o';
         }
         grid[y] = row;
     }
@@ -38,57 +68,56 @@ function Grid(field, numRows, numCols) {
     this.get = grid;
 }
 
-createGrid(userField);
-createGrid(compField);
-
-function Element() {
+// Все возможные корабли
+function Elements() {
+    // Здесь f-часть корабля, n-место вокруг корабля, о-пустое место
     this.single = [['nnn','nfn','nnn']];
     this.twos = [['nnnn','nffn','nnnn'], ['nnn','nfn','nfn','nnn']];
     this.threes = [['nnnnn','nfffn','nnnnn'], ['nnn','nfn','nfn','nfn','nnn'], ['nnnn','nffn','nfnn','nnnn'],
                  ['nnnn','nnfn','nffn','nnnn'], ['nnnn','nffn','nnfn','nnnn'], ['nnnn','nfnn','nffn','nnnn']];
     this.fours = [['nnnnnn','nffffn','nnnnnn'], ['nnn','nfn','nfn','nfn','nfn','nnn'], ['nnnnn','nfffn','nfnnn'],
-                 ['nnnnn','nfffn','nnnfn'],['nnnnn','nnnfn','nfffn'],['nnnnn','nfnnn','nfffn'],['nnnn','nffn','nffn','nnnn'],
-                 ['nnno','nfno','nfnn','nffn','nnnn'],['onnn','onfn','nnfn','nffn','nnnn'],['nnnn','nffn','nnfn','onfn','onnn'],
+                 ['nnnnn','nfffn','nnnfn'], ['nnnnn','nnnfn','nfffn'],['nnnnn','nfnnn','nfffn'], ['nnnn','nffn','nffn','nnnn'],
+                 ['nnno','nfno','nfnn','nffn','nnnn'], ['onnn','onfn','nnfn','nffn','nnnn'], ['nnnn','nffn','nnfn','onfn','onnn'],
                  ['nnnn','nffn','nfnn','nfno','nnno']];
 
 
 }
 
-Element.prototype.getSingles = function() {
+Elements.prototype.getSingles = function() {
     var res = [null,null,null,null];
     return res.map(function(el) { return this.single[0]; });
 };
 
-Element.prototype.getTwos = function() {
+Elements.prototype.getTwos = function() {
     var res = [null,null,null];
     var max = this.twos.length - 1;
     return res.map(function(el) {
-        var randInd = getRandomInt(0, max);
+        var randInd = randomInt(0, max);
         return res.map(function() { return this.twos[randInd]; });
     });
 };
 
-Element.prototype.getThrees = function() {
+Elements.prototype.getThrees = function() {
     var res = [null,null];
     var max = this.threes.length - 1;
     return res.map(function(el) {
-        var randInd = getRandomInt(0, max);
+        var randInd = randomInt(0, max);
         return res.map(function() { return this.twos[randInd]; });
     });
 };
-Element.prototype.getFours = function() {
+Elements.prototype.getFours = function() {
     var max = this.fours.length - 1;
-    var randInd = getRandomInt(0, max);
+    var randInd = randomInt(0, max);
     return [this.fours[randInd]];
 };
 
-function getRandomInt(min, max) {
+function randomInt(min, max) {
   return Math.floor(Math.random() * (max-min+1) + min);
 }
 
-// Находит место для расположения элемента и возвращает x,y координаты начальной точки
-// для "вставки" данного элемента
-function findPlaceForElem(grid, elem) {
+// Находит место для расположения корабля и возвращает x,y координаты начальной точки
+// для "вставки" данного корабля
+function findPlaceForShip(grid, elem) {
     var start_ind = null;
 
     rep:
@@ -104,7 +133,7 @@ function findPlaceForElem(grid, elem) {
     return false;
 }
 
-// Определяет есть ли в строке сетки место для расположения "строчки" данного элемента,
+// Определяет есть ли в строке сетки место для расположения "строчки" данного корабля,
 // если есть, то функция возвращяет индекс с которого начинается свободное место, иначе false.
 // Можно указать индекс с которого начинать проверку - start_ind.
 function findPlaceInRow(rowGrid, rowElem, start_ind) {
@@ -122,6 +151,3 @@ function findPlaceInRow(rowGrid, rowElem, start_ind) {
     }
     return false;
 }
-
-var two_s = [ ["nnnn", "nffn", "nnnn"], ["nnn", "nfn", "nfn", "nnn"] ];
-
